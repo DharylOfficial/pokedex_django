fdee4dab3767ed841f9b31b27a1eb5324b9dbfc3
= <=>
2022-01-20 16:40:21 +0800

    Added app API

--- pokedex_django/pokedex_django/settings.py
+++ pokedex_django/pokedex_django/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pokemon',
     'requests',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- pokedex_django/pokedex_django/urls.py
+++ pokedex_django/pokedex_django/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("pokemon/", include("pokemon.urls")),
+    path("api/", include("api.urls")),
 ]

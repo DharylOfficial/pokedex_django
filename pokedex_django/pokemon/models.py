09dbf3e587634eeabc559e6e84713eba0c5c77f5
= <=>
2022-01-20 14:40:12 +0800

    Create Pokemon model

--- pokedex_django/pokemon/models.py
+++ pokedex_django/pokemon/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Type(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    # pokemon_set
+    
+class Pokemon(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    image_url = models.CharField(max_length=255, null=True, blank=True)
+    types = models.ManyToManyField(Type)
+    
+# class PokemonType(models.Model):
+#     pokemon = models.ForeignKey(Pokemon, null=True, blank=True, on_delete=models.DO_NOTHING)
+#     type = models.ForeignKey(Type, null=True, blank=True, on_delete=models.DO_NOTHING)

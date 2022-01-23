164796595c38c8e1d340ba1f17f2da600dbdf580
= <=>
2022-01-23 16:37:37 +0800

    Install django-cors-headers

--- pokedex_django/pokemon/serializers.py
+++ pokedex_django/pokemon/serializers.py
@@ -9,7 +9,7 @@ class TypeSerializer(serializers.ModelSerializer):
 class PokemonSerializer(serializers.ModelSerializer):
     type_details = TypeSerializer(read_only=True, many=True, source='types')
     
-    # TO UPDATE TYPES, ONLY LIST TYPE IDs ARE NEEDED
+    # TO UPDATE TYPES, ONLY LIST OF TYPE IDs ARE NEEDED
     class Meta:
         model = Pokemon
         fields = ["id", "name", "image_url", "types", "type_details"]
